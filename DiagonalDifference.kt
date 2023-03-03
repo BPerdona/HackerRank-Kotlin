1e7d50542e22b359ac75f5b474463d8fe6864565
Bruno Perdona <bruno15perdona@gmail.com>
2023-03-03 14:10:43 -0300

    DiagonalDiferrence Solved

---
+++ DiagonalDifference.kt
@@ -0,0 +1,31 @@
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.math.absoluteValue
+import kotlin.ranges.*
+import kotlin.text.*
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    val diagonalA = mutableListOf<Int>()
+    val diagonalB = mutableListOf<Int>()
+
+    arr.forEachIndexed{index, array ->
+        diagonalA.add(array[index])
+        diagonalB.add(array[array.size-(index+1)])
+    }
+
+    return (diagonalA.sum() - diagonalB.sum()).absoluteValue
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
